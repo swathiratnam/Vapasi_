e0ccc3f1c9751f2ea66e1338f9abe4cb473ec215
swathibachu <swathibachu@yahoo.com>
2025-07-01 20:48:33 +0530

    Assignment on Multithreading -Ticket booking request

---
+++ MutliThreading_Assignment/src/Main.java
@@ -0,0 +1,19 @@
+import com.multpethreading.TicketBooking;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ExecutorService bookingPool = Executors.newFixedThreadPool(5);
+
+        bookingPool.execute(new TicketBooking("Pooja"));
+        bookingPool.execute(new TicketBooking("Varsha"));
+        bookingPool.execute(new TicketBooking("Rashmi"));
+        bookingPool.execute(new TicketBooking("Pratiba"));
+        bookingPool.execute(new TicketBooking("Laitha"));
+
+        bookingPool.shutdown();
+    }
+}
---
+++ MutliThreading_Assignment/src/com/multpethreading/TicketBooking.java
@@ -0,0 +1,31 @@
+package com.multpethreading;
+
+public class TicketBooking implements Runnable {
+
+    public String threadName;
+
+    public TicketBooking(String threadName) {
+        this.threadName = threadName;
+    }
+
+    public synchronized void booikng() {
+        System.out.println(threadName + " booking received.");
+    }
+
+    public synchronized void payment() {
+        System.out.println(threadName + " payment processed.");
+    }
+
+    public synchronized void ticketConformation() {
+        System.out.println(threadName + " ticket Confirmed.");
+    }
+
+    @Override
+    public void run() {
+
+        booikng();
+        payment();
+        ticketConformation();
+
+    }
+}
