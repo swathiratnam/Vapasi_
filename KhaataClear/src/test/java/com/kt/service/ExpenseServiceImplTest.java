e34b01805d235db675122780735e7ac288be80ef
swathibachu <swathibachu@yahoo.com>
2025-07-06 22:04:53 +0530

    Assignment on Expense Calculator like 'SplitWise' -- test cases

--- KhaataClear/src/test/java/com/kt/service/ExpenseServiceImplTest.java
+++ KhaataClear/src/test/java/com/kt/service/ExpenseServiceImplTest.java
@@ -1,15 +1,5 @@
 package com.kt.service;
 
-import com.kt.util.ExpenseFileReader;
-import org.junit.jupiter.api.Test;
-import org.mockito.MockedStatic;
-
-import java.util.Collections;
-import java.util.Map;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mockStatic;
-
 class ExpenseServiceImplTest {
 
 
--- KhaataClear/src/test/java/com/kt/util/ExpenseFileReaderTest.java
+++ KhaataClear/src/test/java/com/kt/util/ExpenseFileReaderTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.io.InputStream;
 import java.util.List;
 
 class ExpenseFileReaderTest {
