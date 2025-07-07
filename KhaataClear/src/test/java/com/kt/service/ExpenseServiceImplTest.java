477951655b74090d6a41429c5a33c2cea8ecc36e
swathibachu <swathibachu@yahoo.com>
2025-07-07 22:39:06 +0530

    Assignment on Expense Calculator like 'SplitWise' -- test cases

--- KhaataClear/src/test/java/com/kt/service/ExpenseServiceImplTest.java
+++ KhaataClear/src/test/java/com/kt/service/ExpenseServiceImplTest.java
@@ -8,19 +8,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Map;
 
-import com.kt.model.Expenses;
-import com.kt.util.ExpenseFileReader;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-
-import java.util.*;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-
-
 class ExpenseServiceImplTest {
     private ExpenseService expenseService;
 
@@ -31,37 +18,14 @@ class ExpenseServiceImplTest {
     }
 
     @Test
-    void testCalculateExpenses_withSingleExpense() {
-        Expenses expense =mock(Expenses.class);
-        when(expense.getAmount()).thenReturn(100.0);
-        when(expense.getPaidBy()).thenReturn("A");
-        when(expense.getSpentOn()).thenReturn(List.of("A", "B", "C", "D"));
-        List<Expenses> mockExpenseList = Collections.singletonList(expense);
+    public void testCalculateExpenses() {
 
-        try (MockedStatic<ExpenseFileReader> mockedReader = mockStatic(ExpenseFileReader.class)) {
-            mockedReader.when(() -> ExpenseFileReader.readExpenseFromFile("expense.txt")).thenReturn(mockExpenseList);
-
-            Map<String, Double> result = expenseService.calculateExpenses("expense.txt");
-
-            // Each person should owe 25.0
-            // So A paid 100, owes 25 → net +75
-            // B, C, D owe 25 each → net -25
-            assertEquals(4, result.size());
-            assertEquals(75.0, result.get("A"));
-            assertEquals(-25.0, result.get("B"));
-            assertEquals(-25.0, result.get("C"));
-            assertEquals(-25.0, result.get("D"));
-        }
+        Map<String, Double> balance = expenseService.calculateExpenses("expense.txt");
+        assertNotNull(balance);
+        assertEquals(4,balance.size());
+        assertEquals(-75.0,balance.get("A"));
+        assertEquals(25.0,balance.get("D"));
+        assertEquals(325.0,balance.get("B"));
+        assertEquals(-275.0,balance.get("C"));
     }
-    @Test
-    void testCalculateExpenses_NoExpenses() {
-        try (MockedStatic<ExpenseFileReader> mocked = mockStatic(ExpenseFileReader.class)) {
-            mocked.when(() -> ExpenseFileReader.readExpenseFromFile("non_expense.txt")).thenReturn(new ArrayList<>());
-
-            Map<String, Double> result = expenseService.calculateExpenses("non_expense.txt");
-
-            assertTrue(result.isEmpty());
-        }
-    }
-
 }
